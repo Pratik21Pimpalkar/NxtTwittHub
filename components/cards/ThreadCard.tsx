f14886efec727dc6303cd93bc43ff3783abe798e
Pratik Pimpalkar <pratikpimpalkar21@gmail.com>
2023-11-08 16:41:21 +0530

    TimeStamp Added to Twitts

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -28,22 +28,12 @@ interface Props {
   isComment?: boolean;
 }
 
-function ThreadCard({
-  id,
-  currentUserId,
-  parentId,
-  content,
-  author,
-  community,
-  createdAt,
-  comments,
-  isComment,
+function ThreadCard({ id, currentUserId, parentId, content, author, community, createdAt, comments, isComment,
 }: Props) {
   return (
     <article
-      className={`flex w-full flex-col rounded-xl ${
-        isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"
-      }`}
+      className={`flex w-full flex-col rounded-xl ${isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"
+        }`}
     >
       <div className='flex items-start justify-between'>
         <div className='flex w-full flex-1 flex-row gap-4'>
@@ -71,20 +61,10 @@ function ThreadCard({
 
             <div className={`${isComment && "mb-10"} mt-5 flex flex-col gap-3`}>
               <div className='flex gap-3.5'>
-                <Image
-                  src='/assets/heart-gray.svg'
-                  alt='heart'
-                  width={24}
-                  height={24}
-                  className='cursor-pointer object-contain'
+                <Image src='/assets/heart-gray.svg' alt='heart' width={24} height={24} className='cursor-pointer object-contain'
                 />
                 <Link href={`/thread/${id}`}>
-                  <Image
-                    src='/assets/reply.svg'
-                    alt='heart'
-                    width={24}
-                    height={24}
-                    className='cursor-pointer object-contain'
+                  <Image src='/assets/reply.svg' alt='heart' width={24} height={24} className='cursor-pointer object-contain'
                   />
                 </Link>
                 <Image
@@ -143,6 +123,10 @@ function ThreadCard({
           </Link>
         </div>
       )}
+      {community === null &&
+        <p className='mt-5 text-subtle-medium text-gray-1'>
+          {formatDateString(createdAt)}
+        </p>}
 
       {!isComment && community && (
         <Link
